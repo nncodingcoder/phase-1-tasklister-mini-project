96d5e5f37a9a9bb8c5f22df1a0c3ee9ff1f642dc
naomiinyele <inyeleinyele@gmail.com>
2023-09-28 21:48:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+e.preventDefault()
+console.log(document.querySelector('input').value)
+  })
+  
 });
